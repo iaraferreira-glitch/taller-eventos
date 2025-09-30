f85a64475bafffeba053ab839a41fb601105a812
Iara Ferreira <ferreiraiara.10@gmail.com>
2025-09-29 23:28:17 -0300

    La solución al problema del botón y la alerta del div.

--- eventos.js
+++ eventos.js
@@ -1,5 +1,11 @@
 const div = document.getElementById("botoncito");
+const boton = div.querySelector("button");
 
 div.addEventListener("click", () => {
   alert("¡Hola! Soy el div");
+});
+
+boton.addEventListener("click", () => {
+  event.stopPropagation();
+  alert("¡Hola!");
 });
